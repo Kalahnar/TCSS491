8b23493586be86bbfa8d6f7e1a4b507cd3dedebd
Luka Gajic <lukagajic@gmail.com>
2019-03-14 04:53:24 -0700

    Hopefully Works

---
+++ a3/gameengine.js
@@ -0,0 +1,169 @@
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (/* function */ callback, /* DOMElement */ element) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
+function GameEngine() {
+    this.entities = [];
+    this.ctx = null;
+    this.surfaceWidth = null;
+    this.surfaceHeight = null;
+	this.isPaused = true;
+	this.ticked = false;
+	this.isStepping = true;
+	this.play = null;
+	this.pause = null;//this
+	this.step = null;//this
+	this.load = null;
+	this.save = null;
+	this.squares = [];
+}
+
+GameEngine.prototype.init = function (ctx, play, pause, step) {
+    this.ctx = ctx;
+	this.play = play;
+	this.pause = pause;//this
+	this.step = step;//this
+	this.load = load;//add
+	this.save = save;//add
+    this.surfaceWidth = this.ctx.canvas.width;
+    this.surfaceHeight = this.ctx.canvas.height;
+    this.timer = new Timer();
+    this.startInput();
+	
+	this.socket.on("connect", function () {//add
+        console.log("Socket connected.")//add
+    });
+	
+    console.log('game initialized');
+}
+
+GameEngine.prototype.start = function () {
+    console.log("starting game");
+    var that = this;
+	(function gameLoop() {
+		that.loop();
+		requestAnimFrame(gameLoop, that.ctx.canvas);
+	})();
+}
+
+GameEngine.prototype.setSquares = function(squares) {//add this method
+	this.squares = [];
+	for (var i = 0; i < 62; i++) {
+		this.squares.push([]);
+		for (var j = 0; j < 82; j++) {
+			this.squares[i].push(squares[i][j].level);
+		}
+	}
+	this.draw();
+}
+
+
+GameEngine.prototype.pauseGame = function() {
+	console.log("pausing game");
+	this.isPaused = true;
+}
+
+GameEngine.prototype.resumeGame = function() {
+	console.log("resuming game");
+	this.isPaused = false;
+}
+
+GameEngine.prototype.startInput = function () {
+    console.log('Starting input');
+	
+	
+
+    var that = this;
+
+    // event listeners are added here
+
+	this.play.addEventListener("click", function (e) {
+		that.resumeGame();
+    }, false);
+	
+	this.pause.addEventListener("click", function (e) {
+		that.pauseGame();
+    }, false);
+	
+	this.step.addEventListener("click", function (e) {
+		that.isStepping = true;
+    }, false);
+	
+	this.load.addEventListener("click", function (e) {//
+		that.socket.emit('load', { studentname: "Luka Gajic", statename: "save"});//add
+    }, false);//add
+	
+
+    console.log('Input started');
+}
+
+GameEngine.prototype.saveGame = function() {//add
+	this.socket.emit('save', { studentname: "Luka Gajic", statename: "save", state: this.squares });//add
+}//add
+
+GameEngine.prototype.addEntity = function (entity) {
+    console.log('added entity');
+    this.entities.push(entity);
+}
+
+GameEngine.prototype.draw = function () {
+    this.ctx.clearRect(0, 0, this.surfaceWidth, this.surfaceHeight);
+    this.ctx.save();
+    for (var i = 0; i < this.entities.length; i++) {
+        this.entities[i].draw(this.ctx);
+    }
+    this.ctx.restore();
+}
+
+GameEngine.prototype.update = function () {
+    var entitiesCount = this.entities.length;
+
+    for (var i = 0; i < entitiesCount; i++) {
+        var entity = this.entities[i];
+
+        entity.update();
+    }
+}
+
+GameEngine.prototype.loop = function () {
+	if (this.isStepping) {
+		this.update();
+		this.draw();
+		this.isStepping = false;
+	}
+	if (!this.isPaused) {
+		this.clockTick = this.timer.tick();
+		if (this.timer.gameTime % 0.3 > 0.15 && !this.ticked) {
+			this.update();
+			this.draw();
+			this.ticked = true;
+			//console.log("tick");
+		} else if (this.timer.gameTime % 0.3 <= 0.15 && this.ticked){
+			this.ticked = false;
+			//console.log("tock");
+		}
+	}
+}
+
+function Timer() {
+    this.gameTime = 0;
+    this.maxStep = 0.05;
+    this.wallLastTimestamp = 0;
+}
+
+Timer.prototype.tick = function () {
+    var wallCurrent = Date.now();
+    var wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+    this.wallLastTimestamp = wallCurrent;
+
+    var gameDelta = Math.min(wallDelta, this.maxStep);
+    this.gameTime += gameDelta;
+    return gameDelta;
+}
---
+++ a3/main.js
@@ -0,0 +1,90 @@
+var AM = new AssetManager();
+
+AM.queueDownload("./img/knight/IDLE.png");
+
+AM.downloadAll(function () {
+    var canvas = document.getElementById("gameWorld");
+    var ctx = canvas.getContext("2d");
+
+	var playButton = document.getElementById("play");
+	var pauseButton = document.getElementById("pause");
+	var stepButton = document.getElementById("step");
+	var loadButton = document.getElementById("load");//add
+	var saveButton = document.getElementById("save");//add
+	
+    var gameEngine = new GameEngine();
+    gameEngine.init(ctx, playButton, pauseButton, stepButton, loadButton, saveButton);
+    
+	var squares = [[]];
+	/*
+	pushing new squartes into the 2d array 60 * 80
+	*/
+	for (var i = -1; i < 61; i++) { //-1 cuz it kept crashing
+		squares.push([]);
+		for (var j = -1; j < 81; j++) {
+			squares[i+1].push(new Square(gameEngine, j, i));
+		}
+	}
+	
+	/*	
+		initializing edges that arent supposed to show to 0
+		
+		the darker the color the bigger the lvl
+	
+	for (var i = 0; i < 62; i++) {
+		for (var j = 0; j < 82; j++) {
+			if (i === 0 || i === 61) {
+				squares[i][j].level = 0;
+			}
+			if (j === 0 || j === 81) {
+				squares[i][j].level = 0;
+			}
+		}
+	}
+	/*
+		adding each of the squares from the 2d array into the game
+	*/
+	for (var i = 1; i < 61; i++) {
+		for (var j = 1; j < 81; j++) {
+			console.log(i + ", " + j);
+			gameEngine.addEntity(squares[i][j]);
+		}
+	}
+	/*
+		each square in the simulation thats shown on the screen *not the ones on the edges, they all have 8 neighours 
+		up until point clonining the actual game of life
+		setting neighours diagonili as well
+	*/
+	for (var i = 1; i < 61; i++) {
+		for (var j = 1; j < 81; j++) {
+			var neighbors =  [];
+			neighbors.push(squares[i-1][j-1]);
+			neighbors.push(squares[i-1][j]);
+			neighbors.push(squares[i-1][j+1]);
+			neighbors.push(squares[i][j-1]);
+			neighbors.push(squares[i][j+1]);
+			neighbors.push(squares[i+1][j-1]);
+			neighbors.push(squares[i+1][j]);
+			neighbors.push(squares[i+1][j+1]);
+			squares[i][j].setNeighbors(neighbors); //array neighbours, pushed all neighours into the array, then call setNeighbors method which sets passed in array as the neighours of that square
+		}
+	}
+
+	gameEngine.start();
+	
+    console.log("All Done!");
+		saveButton.addEventListener("click", function (e) {
+		gameEngine.setSquares(squares);
+		gameEngine.saveGame();
+    }, false);
+	
+	gameEngine.socket.on("load", function(e) {
+		console.log(e.state);
+		for (var i = 0; i < 62; i++) {
+			for (var j = 0; j < 82; j++) {
+				squares[i][j].level = e.state[i][j];
+			}
+		}
+		gameEngine.setSquares(squares);
+	});//to here add
+});
